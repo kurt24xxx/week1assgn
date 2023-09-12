a1ee88387d75863b37361318b933323ea8f91c84
kurt24xxx <144726028+kurt24xxx@users.noreply.github.com>
2023-09-12 23:17:59 +0000

    week2rework

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-"use strict"
-let myhttp = require('http');
-let myserver = myhttp.createServer( 
-  function( correct ) {
+
+const http = require("http");
+const fs = require("fs").promises;
+const requestListener = function(req, res) {
+  if (req.url === "/") {
+    fs.readFile(__dirname + "/pagex.html").then(contents => {
+      res.setHeader("Content-Type", "text/html; charset(UTF-8");
+      res.writeHead(200);
+      res.end(contents);
+    }
+                                              );
+  }
+  else {
+    fs.readFile(__dirname + "/datax.json").then(contents => {
+      res.setHeader("Content-Type", "application/json; charset(UTF-8");
+      res.writeHead(200);
+      res.end(contents);
+    });
+  }
+    };
   
-  let mytext;
-    if ( correct.url === "yes" ) {
-      mytext = "True";
-    } else {
-      mytext = "False";
-    }});
+  
+
+
+const serverx = http.createServer( requestListener);
+
+  const host = "0.0.0.0";
+const port = "8080";
+
+serverx.listen(port,host, () => {
+  console.log("running");
+});
